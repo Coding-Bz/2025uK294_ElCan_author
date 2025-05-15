ad29d2744ed6030b88d8cac3a3a69d6a421a0466
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-05-15 14:47:06 +0200

    20-üK294
    Installing the needed dependencies.

--- LoginPage/src/App.tsx
+++ LoginPage/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const shoot = () => {
+    alert(email+""+password);
+  }
+  const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="LoginPageBox">
+      <h1>Login</h1>
+
+      <div id="Email">
+        <h2>Email</h2>
+        <form>
+          <input type="email" placeholder="Enter email" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div id="Passwort">
+        <h2>Passwort</h2>
+        <input type="password" placeholder="Enter Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <button onClick={shoot}>Login</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
