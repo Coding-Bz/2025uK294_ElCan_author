79f709badc918f4a8b5f90b4567b76c916d8a410
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-05-16 12:55:20 +0200

    20-üK294
    This is the login page the functionality is existence but the design is going to be implemnted after I combine the branch List and Login.

--- LoginPage/src/App.tsx
+++ LoginPage/src/App.tsx
@@ -1,31 +1,8 @@
-import { useState } from 'react';
-import './App.css';
+import React from "react";
+import LoginPage from "./LoginPage";
 
-function App() {
-  const shoot = () => {
-    alert(email+""+password);
-  }
-  const [email, setEmail]=useState('');
-    const [password, setPassword]=useState('');
-
-  return (
-    <div id="LoginPageBox">
-      <h1>Login</h1>
-
-      <div id="Email">
-        <h2>Email</h2>
-        <form>
-          <input type="email" placeholder="Enter email" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
-        </form>
-      </div>
-      <div id="Passwort">
-        <h2>Passwort</h2>
-        <input type="password" placeholder="Enter Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-      </div>
-
-      <button onClick={shoot}>Login</button>
-    </div>
-  );
-}
+const App = () => {
+  return <LoginPage />;
+};
 
 export default App;
